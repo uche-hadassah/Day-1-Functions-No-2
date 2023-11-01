af8e75c008f57db887ba3d280e08d2aca3d07677
Hadassah Uche <uhadassah1@gmail.com>
2023-11-01 13:13:51 +0200

    Skeleton done

---
+++ UcheHadassah2_2/HadassahUche2_2.cpp
@@ -0,0 +1,16 @@
+/*Write a function called power that takes as parameters one double x and one integer n
+and returns the value of x raised to the power n. (Do not use the built-in function 
+pow). Test your function.
+For example: power(2.0, 3) should return 8 (i.e. 2.0*2.0*2.0*/
+#include <iostream>
+using namespace std;
+int power(double, int);//Function Prototype
+int main()
+{
+
+	return 0;
+}
+int power(double, int)
+{
+
+}
